a90173209a5481550eb9e31dbce85994811a106b
zhangbo28 <zhangbo28@lenovo.com>
2017-12-04 16:59:54 +0800

    修改部分逻辑

--- src/api/api.js
+++ src/api/api.js
@@ -6,8 +6,8 @@ export const requestLogin = params => { return axios.post(`${base}/login`, param
 
 export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
 
-export const getAskQPage = params => { return axios.get('http://bird.ioliu.cn/v1/?url=http://rap2api.taobao.org/app/mock/1019/GET/getAskAnsList', { params: params }); };
-
+export const getAskQPage = params => { return axios.get('http://10.116.19.131/robot/questions.do', { params: params }); };
+// http://bird.ioliu.cn/v1/?url=
 // export const getAskQPage = params => { return axios.get('http://localhost:9998/data2.json', { params: params }) };
 
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,10 +12,12 @@
 			<!-- 左侧的菜单 -->
 			<aside class="aside">
 				<!-- <AsideLf></AsideLf> -->
-				<el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+
+				<!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
 					<el-radio-button :label="false">展开</el-radio-button>
 					<el-radio-button :label="true">收起</el-radio-button>
-				</el-radio-group>
+				</el-radio-group> -->
+				
 				<el-menu 
 					:default-active="$route.path" 
 					class="el-menu-vertical-demo el-menu-ay" 
